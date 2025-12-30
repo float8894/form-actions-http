8ea3e995ddae97384d96a4d251deb46ec62424fc
Abhishek Panchal <abhishekpanchal8894@gmail.com>
2025-12-30 19:11:39 +0530

    registering multiple form actions

--- src/components/Opinion.jsx
+++ src/components/Opinion.jsx
@@ -1,4 +1,15 @@
+import { useContext } from 'react';
+import { OpinionsContext } from '../store/opinions-context';
+
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
+  const { upvoteOpinion, downvoteOpinion } = useContext(OpinionsContext);
+  function upvoteAction() {
+    upvoteOpinion(id);
+  }
+
+  function downvoteAction() {
+    downvoteOpinion(id);
+  }
   return (
     <article>
       <header>
@@ -6,42 +17,42 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
         <p>Shared by {userName}</p>
       </header>
       <p>{body}</p>
-      <form className="votes">
-        <button>
+      <form className='votes'>
+        <button formAction={upvoteAction}>
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
           >
-            <rect width="18" height="18" x="3" y="3" rx="2" />
-            <path d="m16 12-4-4-4 4" />
-            <path d="M12 16V8" />
+            <rect width='18' height='18' x='3' y='3' rx='2' />
+            <path d='m16 12-4-4-4 4' />
+            <path d='M12 16V8' />
           </svg>
         </button>
 
         <span>{votes}</span>
 
-        <button>
+        <button formAction={downvoteAction}>
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
           >
-            <rect width="18" height="18" x="3" y="3" rx="2" />
-            <path d="M12 8v8" />
-            <path d="m8 12 4 4 4-4" />
+            <rect width='18' height='18' x='3' y='3' rx='2' />
+            <path d='M12 8v8' />
+            <path d='m8 12 4 4 4-4' />
           </svg>
         </button>
       </form>
